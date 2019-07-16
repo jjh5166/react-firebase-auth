d017892692669976a2f81f23baaec9b6c4882888
John <jjh5166@gmail.com>
2019-07-16 00:44:26 -0400

    sign out

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-const SignOut = () => (<div>
-  <h1>SignOut</h1>
-</div>
-);
-export default SignOut;
+import { withFirebase } from '../Firebase';
+
+const SignOutButton = ({ firebase }) => (
+  <button type="button" onClick={firebase.doSignOut}>
+    Sign Out
+  </button>);
+
+export default withFirebase(SignOutButton);
