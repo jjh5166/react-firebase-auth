3bdaaa1f048642cbec49f85eb706e22fbdaf7001
John <jjh5166@gmail.com>
2019-08-02 17:28:38 -0400

    password change

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-const Account = () => (<div>
-  <h1>Account</h1>
-</div>
-);
-export default Account;
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
+const AccountPage = () => (<div>
+  <h1>Account Page</h1>
+  <PasswordForgetForm />
+  <PasswordChangeForm />
+</div>);
+export default AccountPage;
